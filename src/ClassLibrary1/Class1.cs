943043b07e46ac714fc535a4aba513cd38a8f970
Robert Sargéus <robert@sargeus.se>
2014-11-08 22:48:40 +0000

    First commit

---
+++ src/ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+
+        public static string GetMessage()
+        {
+            return "New message from ClassLibrary1!";
+        }
+
+    }
+}
---
+++ src/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+            ViewBag.Message = ClassLibrary1.Class1.GetMessage();
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
